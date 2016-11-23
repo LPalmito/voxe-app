067c726d0c3e2f6878d59a078b4487d941e10403
L Palmito <yannickmorel1@gmail.com>
2016-11-23 11:34:19 +0100

    Implemented a first test for the swipe feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import {NgModule} from "@angular/core";
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {IonicApp, IonicModule} from "ionic-angular";
 import {VoxeApp} from "./app.component";
 import {HomePage} from "../pages/home/home";
 import {SwipePage} from "../pages/swipe/swipe";
-import {SwingStackComponent} from "angular2-swing";
-import {SwingCardComponent} from "angular2-swing";
+import {StatsPage} from "../pages/stats/stats";
 
 @NgModule({
   declarations: [
     VoxeApp,
     HomePage,
     SwipePage,
-    SwingStackComponent,
-    SwingCardComponent
+    StatsPage
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     IonicModule.forRoot(VoxeApp)
   ],
@@ -23,7 +19,8 @@ import {SwingCardComponent} from "angular2-swing";
   entryComponents: [
     VoxeApp,
     HomePage,
-    SwipePage
+    SwipePage,
+    StatsPage
   ],
   providers: []
 })
