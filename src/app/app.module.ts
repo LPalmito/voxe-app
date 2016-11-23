f22410040ce1e58f0b4da80b981146cf4a600feb
L Palmito <yannickmorel1@gmail.com>
2016-11-23 11:33:08 +0100

    Implemented a first test for the swipe feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import {NgModule} from "@angular/core";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {IonicApp, IonicModule} from "ionic-angular";
 import {VoxeApp} from "./app.component";
 import {HomePage} from "../pages/home/home";
 import {SwipePage} from "../pages/swipe/swipe";
+import {SwingStackComponent} from "angular2-swing";
+import {SwingCardComponent} from "angular2-swing";
 
 @NgModule({
   declarations: [
     VoxeApp,
     HomePage,
-    SwipePage
+    SwipePage,
+    SwingStackComponent,
+    SwingCardComponent
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     IonicModule.forRoot(VoxeApp)
   ],
--- src/pages/swipe/swipe.ts
+++ src/pages/swipe/swipe.ts
@@ -1,11 +1,107 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild, ViewChildren, QueryList, NgModule} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/Rx';
+import {
+  StackConfig,
+  Stack,
+  Card,
+  ThrowEvent,
+  DragEvent,
+  SwingStackComponent,
+  SwingCardComponent} from 'angular2-swing';
 
+//!\\
+// When launching "npm install angular2-swing@^0.7.1 --save"
+// npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@^1.0.0 (node_modules\chokidar\node_modules\fsevents):
+// npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@1.0.15: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
 
+// 'directives' does not exist in 'Component', use 'declarations' in 'NgModule' instead:
+// http://stackoverflow.com/questions/39886792/directive-does-not-exist-in-type-component
 @Component({
-  templateUrl: 'swipe.html'
+  templateUrl: 'swipe.html',
 })
-export class SwipePage {
-  constructor() {
 
+export class SwipePage {
+  @ViewChild('myswing1') swingStack: SwingStackComponent;
+  @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
+
+  cards: Array<any>;
+  stackConfig: StackConfig;
+  recentCard: string = '';
+
+  constructor(private http: Http) {
+    this.stackConfig = {
+      throwOutConfidence: (offset, element) => {
+        return Math.min(Math.abs(offset) / (element.offsetWidth/2), 1);
+      },
+      transform: (element, x, y, r) => {
+        SwipePage.onItemMove(element, x, y, r);
+      },
+      throwOutDistance: (d) => {
+        return 800;
+      }
+    };
   }
+
+  ngAfterViewInit() {
+    // Either subscribe in controller or set in HTML
+    this.swingStack.throwin.subscribe((event: DragEvent) => {
+      event.target.style.background = '#ffffff';
+    });
+
+    this.cards = [{email: ''}];
+    this.addNewCards(1);
+  }
+
+  // Called whenever we drag an element
+  static onItemMove(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(x);
+    let min = Math.floor(Math.min(16*16 - abs, 16*16));
+    let hexCode = SwipePage.decimalToHex(min, 2);
+
+    if (x < 0) {
+      color = '#FF' + hexCode + hexCode;
+    } else {
+      color = '#' + hexCode + 'FF' + hexCode;
+    }
+
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+
+  // Connected through HTML
+  voteUp(like: boolean) {
+    let removedCard = this.cards.pop();
+    this.addNewCards(1);
+    if (like) {
+      this.recentCard = 'You liked: ' + removedCard.email;
+    } else {
+      this.recentCard = 'You disliked: ' + removedCard.email;
+    }
+  }
+
+  // Add new cards to our array
+  addNewCards(count: number) {
+    this.http.get('https://randomuser.me/api/?results=' + count)
+      .map(data => data.json().results)
+      .subscribe(result => {
+        for (let val of result) {
+          this.cards.push(val);
+        }
+      })
+  }
+
+  // http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
+  static decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+      hex = "0" + hex;
+    }
+
+    return hex;
+  }
+
 }
