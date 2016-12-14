34461cb2959a3bb5d08c27c972879ef242637b5b
Gus <augustin.courtier@gmail.com>
2016-12-14 16:08:34 +0100

    Used ionic 2 templates to build proposition summary

--- src/pages/stats/stats.ts
+++ src/pages/stats/stats.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {Answer} from "../swipe/swipe";
 import {NavParams} from "ionic-angular";
+import {NavController} from "ionic-angular";
 
 @Component({
   templateUrl: 'stats.html'
