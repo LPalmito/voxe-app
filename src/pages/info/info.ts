15f78aa29b5fd49b92b61f1d99695040cc173396
Gus <augustin.courtier@gmail.com>
2017-03-18 11:43:21 +0100

    Rebased with master. Some commented code in src/pages/info --> for star/unstar cards (not working)

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -2,7 +2,23 @@ import {Component} from "@angular/core";
 import {Store} from "@ngrx/store";
 import {AppStore} from "../../store";
 import {MainService} from "../../services/main.service";
-
+// import {HomePage} from "../home/home";
+//
+// export enum CardType {
+//   Info,
+//   Swipe
+// }
+//
+// export class Card {
+// 	image: string;
+// 	isStar: boolean;
+// 	isArchive: boolean;
+// }
+//
+// export class InfoCard extends Card {
+// 	infoUrl: string[];
+// 	type: CardType = CardType.Info;
+// }
 
 @Component({
   templateUrl: 'info.html'
