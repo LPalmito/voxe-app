edcffb193fd3df56ad246c25ba34a4112158f6e5
Manon-L <manon.leger@ntymail.com>
2017-03-19 01:37:44 +0100

    Implemented a test iframe. Bypassed security alerts & Removed header. Did not succeed yet in removing footer.
    Signed-off-by: Manon-L <manon.leger@ntymail.com>

--- src/pages/scrap/scrap.ts
+++ src/pages/scrap/scrap.ts
@@ -1,14 +1,14 @@
 import {Component} from "@angular/core";
-import {DomSanitizer} from '@angular/platform-browser';
-import {SafeUrl} from '@angular/platform-browser';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   templateUrl: 'scrap.html'
 })
 
 export class ScrapPage {
-  scrap: SafeUrl = this.sanitizer.bypassSecurityTrustUrl("http://www.voxe.org/jean-luc-bennahmias/");
+  scrap: SafeResourceUrl;
 
   constructor(public sanitizer: DomSanitizer) {
+    this.scrap = this.sanitizer.bypassSecurityTrustResourceUrl("http://www.voxe.org/jean-luc-bennahmias/");
   }
 }
